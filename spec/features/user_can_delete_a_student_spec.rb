4603f6222f583c13d9072d63d35bfae379396783
slimecog <wlcjohnson@gmail.com>
2018-02-07 14:51:23 -0700

    upgrade student delete test

--- spec/features/user_can_delete_a_student_spec.rb
+++ spec/features/user_can_delete_a_student_spec.rb
@@ -2,14 +2,15 @@ require "rails_helper"
 
 describe "a user deletes a student" do
   scenario "a user can delete an exiting user" do
-    student = Student.create!(name: "bill")
+    student = Student.create!(name: "Bill")
+    student2 = Student.create!(name: "billy")
     visit students_path
 
     within(".student_#{student.id}") do
       click_link "Delete"
     end
-
     expect(current_path).to eq(students_path)
     expect(page).to_not have_content(student.name)
+    expect(page).to have_content(student2.name)
   end
 end
